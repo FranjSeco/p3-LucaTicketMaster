728ff116f86d8d50ea8c8cbf3f541f7b8fbe5c3a
unknown <franjseco@gmail.com>
2022-01-25 10:52:40 +0100

    structure

---
+++ P3-EventService/src/main/java/com/example/demo/P3EventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3EventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3EventServiceApplication.class, args);
+	}
+
+}
