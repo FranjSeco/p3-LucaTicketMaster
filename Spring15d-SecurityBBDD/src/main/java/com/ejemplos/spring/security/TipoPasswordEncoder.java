037491a4235495c0d2c49ef0e0fd6515a5e97213
Miguel <93081004+Apulsar@users.noreply.github.com>
2022-01-27 13:21:30 +0100

    Delete Spring15d-SecurityBBDD directory

--- Spring15d-SecurityBBDD/src/main/java/com/ejemplos/spring/security/TipoPasswordEncoder.java
+++
@@ -1,7 +0,0 @@
-package com.ejemplos.spring.security;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-public interface TipoPasswordEncoder extends PasswordEncoder {
-
-}
--- Spring15d-SecurityBBDD/src/main/java/com/ejemplos/spring/services/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.ejemplos.spring.services;
-
-import com.ejemplos.spring.model.User;
-
-public interface UserService {
-	public User findUserByUsername(String username);
-	public User findUserByEmail(String email);
-	public void saveUser(User user);
-}
