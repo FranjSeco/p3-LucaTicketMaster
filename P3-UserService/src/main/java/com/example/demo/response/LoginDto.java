e78f2d80147e6c41b3a8a000859bd5c7845a92a8
unknown <franjseco@gmail.com>
2022-01-27 13:38:10 +0100

    fixing merges

--- P3-UserService/src/main/java/com/example/demo/response/LoginDto.java
+++ P3-UserService/src/main/java/com/example/demo/response/LoginDto.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 package com.example.demo.response;
 
 
@@ -32,71 +31,4 @@ public class LoginDto {
     
 
 }
-=======
-package com.example.demo.response;
 
-
-public class LoginDto {
-	
-	private String usernameOrEmail;
-    private String password;
-    
-    
-	public LoginDto() {
-		super();
-	}
-	public String getUsernameOrEmail() {
-		return usernameOrEmail;
-	}
-	public void setUsernameOrEmail(String usernameOrEmail) {
-		this.usernameOrEmail = usernameOrEmail;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	@Override
-	public String toString() {
-		return "LoginDto [usernameOrEmail=" + usernameOrEmail + ", password=" + password + "]";
-	}
-    
-    
-
-}
->>>>>>> 1a358005aa76882c22b3c6ccafde611b89e267e3
-=======
-package com.example.demo.response;
-
-
-public class LoginDto {
-	
-	private String usernameOrEmail;
-    private String password;
-    
-    
-	public LoginDto() {
-		super();
-	}
-	public String getUsernameOrEmail() {
-		return usernameOrEmail;
-	}
-	public void setUsernameOrEmail(String usernameOrEmail) {
-		this.usernameOrEmail = usernameOrEmail;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	@Override
-	public String toString() {
-		return "LoginDto [usernameOrEmail=" + usernameOrEmail + ", password=" + password + "]";
-	}
-    
-    
-
-}
->>>>>>> 1a358005aa76882c22b3c6ccafde611b89e267e3
