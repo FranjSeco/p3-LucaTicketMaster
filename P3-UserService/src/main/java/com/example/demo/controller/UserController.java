9d903f61d2cecc30cb932170732107f66b59d804
luvallejo <lufevallejo@hotmail.com>
2022-01-28 08:53:04 +0100

    Probado Eureka, añadido de errores UserService

--- P3-UserService/src/main/java/com/example/demo/controller/UserController.java
+++ P3-UserService/src/main/java/com/example/demo/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.controller.error.UserNotFoundException;
 import com.example.demo.model.User;
 import com.example.demo.services.UserService;
 
