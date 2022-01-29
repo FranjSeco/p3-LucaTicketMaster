cd45a3f3311bedcd91f3d9065a9f019d351c4d21
luvallejo <lufevallejo@hotmail.com>
2022-01-29 17:28:29 +0100

    método delete

--- P3-UserService/src/main/java/com/example/demo/controller/UserController.java
+++ P3-UserService/src/main/java/com/example/demo/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -116,6 +117,17 @@ public class UserController {
 		}
 	}
 
+	
+	@DeleteMapping("/{username}")
+	public User deleteUser(@PathVariable String username) {
+		
+		userService.deleteUser(username);
+		return null;
+		
+		
+	}
+	
+	
 	
 
 }
