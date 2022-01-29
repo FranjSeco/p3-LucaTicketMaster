79f01ab0496da874b9e2ecbecb1f91e5a8ae93f9
unknown <franjseco@gmail.com>
2022-01-29 18:22:11 +0100

    testing-mongo-fran

--- P3-EventService2/src/test/java/com/example/demo/P3EventService2ApplicationTests.java
+++ P3-EventService2/src/test/java/com/example/demo/P3EventService2ApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.service.EventService;
 
 @SpringBootTest
 class P3EventService2ApplicationTests {
+	
+	@Autowired
+	EventService eventServiceTest;
+	
 
 	@Test
 	void contextLoads() {
+		assertFalse(eventServiceTest.showAllEvents().isEmpty());
 	}
 
 }
