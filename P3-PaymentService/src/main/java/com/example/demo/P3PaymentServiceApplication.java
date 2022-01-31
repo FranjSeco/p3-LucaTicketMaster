1fc6f965aaab277e71a8b5d2551f29c02068e9f9
unknown <franjseco@gmail.com>
2022-01-31 09:55:51 +0100

    tickets and payments

---
+++ P3-PaymentService/src/main/java/com/example/demo/P3PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3PaymentServiceApplication.class, args);
+	}
+
+}
